d239d5dc3ad53d4cd1a034026af07b114ecbd45a
Teo Han <meteory.kr@gmail.com>
2026-05-09 10:33:16 +0900

    refactor: rename project

--- SetSpeedShortcuts/Properties/AssemblyInfo.cs
+++ SetSpeedShortcuts/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ADOFAIMod2")]
+[assembly: AssemblyTitle("SetSpeedShortcuts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ADOFAIMod2")]
+[assembly: AssemblyProduct("SetSpeedShortcuts")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
