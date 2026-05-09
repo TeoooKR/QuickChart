90abcfa9317b4a4d207dc4b91d0444032e85be5e
Teo Han <meteory.kr@gmail.com>
2026-05-09 11:48:48 +0900

    feat: add bpm +1/-1

--- SetSpeedShortcuts/Main.cs
+++ SetSpeedShortcuts/Main.cs
@@ -18,18 +18,23 @@ public static class Main
         modEntry.OnToggle = OnToggle;
     }
     static public float MULTIPLIER = 1;
+    static public float BEATS_PER_MINUTE = 0;
     static public bool IS_ARROW = false;
+    static public bool IS_TYPE_MULTIPLIER = false;
     private static void OnUpdate(UnityModManager.ModEntry modEntry, float deltaTime) {
         if (!IsEnabled) return;
 
-        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.UpArrow)) {
-            HandleSpeedChange(2.0f);
-        }
-        else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.DownArrow)) {
-            HandleSpeedChange(0.5f);
+        if (Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow)) {
+            HandleSpeedMultiply(2.0f);
+        } else if (Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.DownArrow)) {
+            HandleSpeedMultiply(0.5f);
+        } else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.UpArrow)) {
+            HandleSpeedOne(1);
+        } else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.DownArrow)) {
+            HandleSpeedOne(-1);
         }
     }
-    private static void HandleSpeedChange(float ratio) {
+    private static void HandleSpeedMultiply(float ratio) {
         var selectedEvents = scnEditor.instance.GetSelectedFloorEvents(LevelEventType.SetSpeed);
         
         if (selectedEvents != null && selectedEvents.Count > 0) {
@@ -49,11 +54,45 @@ public static class Main
             
             scnEditor.instance.ApplyEventsToFloors(); //update tiles
         } else {
-            MULTIPLIER = ratio; 
+            IS_TYPE_MULTIPLIER = true;
+            MULTIPLIER = ratio;
             IS_ARROW = true;
             scnEditor.instance.AddEventAtSelected(LevelEventType.SetSpeed);
         }
     }
+    
+    private static void HandleSpeedOne(float delta) {
+        var selectedEvents = scnEditor.instance.GetSelectedFloorEvents(LevelEventType.SetSpeed);
+        
+        if (selectedEvents != null && selectedEvents.Count > 0) {
+            LevelEvent ev = selectedEvents[0];
+            string type = ev.data["speedType"].ToString();
+            
+            if (type == "Multiplier" || type == "1") {
+                return;
+            } 
+            else if (type == "Bpm") {
+                float currentBpm = System.Convert.ToSingle(ev.data["beatsPerMinute"]);
+                if (currentBpm + delta > 0) {
+                    ev.data["beatsPerMinute"] = currentBpm + delta;
+                } else {
+                    ev.data["beatsPerMinute"] = currentBpm;
+                }
+                scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "beatsPerMinute"); //update input
+            }
+            scnEditor.instance.ApplyEventsToFloors(); //update tiles
+        } else {
+            IS_TYPE_MULTIPLIER = false;
+            float currentBpm = ADOBase.editor.levelData.bpm * ADOBase.editor.selectedFloors[0].speed;
+            BEATS_PER_MINUTE = currentBpm;
+            if (currentBpm + delta > 0) {
+                BEATS_PER_MINUTE += delta;
+            }
+            IS_ARROW = true;
+            scnEditor.instance.AddEventAtSelected(LevelEventType.SetSpeed);
+        }
+    }
+    
     private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
         IsEnabled = value;
         if (value) {
--- SetSpeedShortcuts/SetSpeed.cs
+++ SetSpeedShortcuts/SetSpeed.cs
@@ -6,8 +6,12 @@ public static class AddEventPatch {
     static void Postfix(scnEditor __instance, LevelEventType eventType) {
         if (eventType == LevelEventType.SetSpeed && Main.IS_ARROW) {
             LevelEvent lastEvent = __instance.events[__instance.events.Count - 1];
-            lastEvent.data["bpmMultiplier"] = Main.MULTIPLIER;
-            lastEvent.data["speedType"] = 1; // idk why this works lol
+            if (Main.IS_TYPE_MULTIPLIER) {
+                lastEvent.data["bpmMultiplier"] = Main.MULTIPLIER;
+                lastEvent.data["speedType"] = 1; // idk why this works lol
+            } else if(!Main.IS_TYPE_MULTIPLIER) {
+                lastEvent.data["beatsPerMinute"] = Main.BEATS_PER_MINUTE;
+            }
             Main.IS_ARROW = false;
         }
     }
