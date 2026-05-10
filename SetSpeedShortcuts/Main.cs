bfffb04b9ab05762ba264bd141732c38750204d7
Teo Han <meteory.kr@gmail.com>
2026-05-10 12:42:46 +0900

    feat: if same speed delete event

--- SetSpeedShortcuts/Main.cs
+++ SetSpeedShortcuts/Main.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using ADOFAI;
 using HarmonyLib;
@@ -7,8 +8,12 @@ using UnityEngine;
 public static class Main
 {
     public static UnityModManager.ModEntry.ModLogger Logger;
-    public static Harmony harmony;
-    public static bool IsEnabled = false;
+    public static Harmony Harmony;
+    public static bool IsEnabled;
+
+    static float _keyHoldTimer;
+    static float _repeatTimer;
+
     public static void Setup(UnityModManager.ModEntry modEntry)
     {
         modEntry.Logger.Log("Setup!");
@@ -17,46 +22,89 @@ public static class Main
         modEntry.OnUpdate = OnUpdate; 
         modEntry.OnToggle = OnToggle;
     }
-    static public float MULTIPLIER = 1;
-    static public float BEATS_PER_MINUTE = 0;
-    static public bool IS_ARROW = false;
-    static public bool IS_TYPE_MULTIPLIER = false;
+    static public float Multiplier = 1;
+    static public float BeatsPerMinute;
+    static public bool IsArrow;
+    static public bool IsTypeMultiplier;
+
     private static void OnUpdate(UnityModManager.ModEntry modEntry, float deltaTime) {
         if (!IsEnabled) return;
 
+        if (Input.GetKeyDown(KeyCode.F4)) {
+        }
+        
         if (Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow)) {
             HandleSpeedMultiply(2.0f);
         } else if (Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.DownArrow)) {
             HandleSpeedMultiply(0.5f);
-        } else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.UpArrow)) {
-            HandleSpeedOne(1);
-        } else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.DownArrow)) {
-            HandleSpeedOne(-1);
+        } 
+        else if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl)) {
+            bool up = Input.GetKey(KeyCode.UpArrow);
+            bool down = Input.GetKey(KeyCode.DownArrow);
+            if (up || down) {
+                float delta = up ? 1 : -1;
+                if (_keyHoldTimer == 0f) {
+                    HandleSpeedOne(delta);
+                    _keyHoldTimer += deltaTime;
+                } else {
+                    _keyHoldTimer += deltaTime;
+                    if (_keyHoldTimer > 0.4f) {
+                        _repeatTimer += deltaTime;
+                        if (_repeatTimer > 0.05f) {
+                            HandleSpeedOne(delta);
+                            _repeatTimer = 0f;
+                        }
+                    }
+                }
+            } else {
+                _keyHoldTimer = 0f;
+                _repeatTimer = 0f;
+            }
+        } else {
+            _keyHoldTimer = 0f;
+            _repeatTimer = 0f;
         }
     }
+
     private static void HandleSpeedMultiply(float ratio) {
         var selectedEvents = scnEditor.instance.GetSelectedFloorEvents(LevelEventType.SetSpeed);
-        
+        if (!scnEditor.instance.SelectionIsSingle()) {
+            return;
+        }
         if (selectedEvents != null && selectedEvents.Count > 0) {
             LevelEvent ev = selectedEvents[0];
             string type = ev.data["speedType"].ToString();
             
             if (type == "Multiplier" || type == "1") {
                 float currentMult = System.Convert.ToSingle(ev.data["bpmMultiplier"]);
-                ev.data["bpmMultiplier"] = currentMult * ratio;
-                scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "bpmMultiplier"); //update input
+                if (Mathf.Approximately(currentMult * ratio, 1)) {
+                    scnEditor.instance.RemoveEvents(new List<LevelEvent> { ev });
+                } else {
+                    ev.data["bpmMultiplier"] = currentMult * ratio;
+                    scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "bpmMultiplier"); //update input    
+                }
             } 
             else if (type == "Bpm") {
                 float currentBpm = System.Convert.ToSingle(ev.data["beatsPerMinute"]);
-                ev.data["beatsPerMinute"] = currentBpm * ratio;
-                scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "beatsPerMinute"); //update input
+                int previousTile = ADOBase.editor.selectedFloors[0].seqID - 1;
+                float previousBpm = ADOBase.editor.levelData.bpm;
+
+                if (previousTile > 0) {
+                    previousBpm *= ADOBase.editor.floors[previousTile].speed;
+                }
+                if (Mathf.Approximately(currentBpm * ratio, previousBpm)) {
+                    scnEditor.instance.RemoveEvents(new List<LevelEvent> { ev });
+                } else {
+                    ev.data["beatsPerMinute"] = currentBpm * ratio;
+                    scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "beatsPerMinute"); //update input
+                }
             }
-            
             scnEditor.instance.ApplyEventsToFloors(); //update tiles
+            scnEditor.instance.levelEventsPanel.ShowPanel(LevelEventType.SetSpeed);
         } else {
-            IS_TYPE_MULTIPLIER = true;
-            MULTIPLIER = ratio;
-            IS_ARROW = true;
+            IsTypeMultiplier = true;
+            Multiplier = ratio;
+            IsArrow = true;
             scnEditor.instance.AddEventAtSelected(LevelEventType.SetSpeed);
         }
     }
@@ -73,22 +121,33 @@ public static class Main
             } 
             else if (type == "Bpm") {
                 float currentBpm = System.Convert.ToSingle(ev.data["beatsPerMinute"]);
-                if (currentBpm + delta > 0) {
-                    ev.data["beatsPerMinute"] = currentBpm + delta;
-                } else {
-                    ev.data["beatsPerMinute"] = currentBpm;
+                int previousTile = ADOBase.editor.selectedFloors[0].seqID - 1;
+                float previousBpm = ADOBase.editor.levelData.bpm;
+
+                if (previousTile > 0) {
+                    previousBpm *= ADOBase.editor.floors[previousTile].speed;
                 }
-                scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "beatsPerMinute"); //update input
+                if (Mathf.Approximately(currentBpm + delta, previousBpm)) {
+                    scnEditor.instance.RemoveEvents(new List<LevelEvent> { ev });
+                } else {
+                    if (currentBpm + delta > 0) {
+                        ev.data["beatsPerMinute"] = currentBpm + delta;
+                    } else {
+                        ev.data["beatsPerMinute"] = currentBpm;
+                    }
+                    scnEditor.instance.levelEventsPanel.UpdatePropertyText(ev, "beatsPerMinute"); //update input
+                }
+
             }
             scnEditor.instance.ApplyEventsToFloors(); //update tiles
         } else {
-            IS_TYPE_MULTIPLIER = false;
+            IsTypeMultiplier = false;
             float currentBpm = ADOBase.editor.levelData.bpm * ADOBase.editor.selectedFloors[0].speed;
-            BEATS_PER_MINUTE = currentBpm;
+            BeatsPerMinute = currentBpm;
             if (currentBpm + delta > 0) {
-                BEATS_PER_MINUTE += delta;
+                BeatsPerMinute += delta;
             }
-            IS_ARROW = true;
+            IsArrow = true;
             scnEditor.instance.AddEventAtSelected(LevelEventType.SetSpeed);
         }
     }
@@ -96,11 +155,11 @@ public static class Main
     private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
         IsEnabled = value;
         if (value) {
-            harmony = new Harmony(modEntry.Info.Id);
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Harmony = new Harmony(modEntry.Info.Id);
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
         else { 
-            harmony.UnpatchAll(modEntry.Info.Id);
+            Harmony.UnpatchAll(modEntry.Info.Id);
         }
         return true;
     }
--- SetSpeedShortcuts/SetSpeed.cs
+++ SetSpeedShortcuts/SetSpeed.cs
@@ -4,15 +4,15 @@ using HarmonyLib;
 [HarmonyPatch(typeof(scnEditor), "AddEvent")]
 public static class AddEventPatch {
     static void Postfix(scnEditor __instance, LevelEventType eventType) {
-        if (eventType == LevelEventType.SetSpeed && Main.IS_ARROW) {
+        if (eventType == LevelEventType.SetSpeed && Main.IsArrow) {
             LevelEvent lastEvent = __instance.events[__instance.events.Count - 1];
-            if (Main.IS_TYPE_MULTIPLIER) {
-                lastEvent.data["bpmMultiplier"] = Main.MULTIPLIER;
+            if (Main.IsTypeMultiplier) {
+                lastEvent.data["bpmMultiplier"] = Main.Multiplier;
                 lastEvent.data["speedType"] = 1; // idk why this works lol
-            } else if(!Main.IS_TYPE_MULTIPLIER) {
-                lastEvent.data["beatsPerMinute"] = Main.BEATS_PER_MINUTE;
+            } else if(!Main.IsTypeMultiplier) {
+                lastEvent.data["beatsPerMinute"] = Main.BeatsPerMinute;
             }
-            Main.IS_ARROW = false;
+            Main.IsArrow = false;
         }
     }
 }
